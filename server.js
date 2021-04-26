0cd96a0bf31cfc96277cedb8f516a2123510d968
clsrns111 <clsrns1111@gmail.com>
2021-04-26 17:15:14 +0900

    비밀정보 보호

--- server.js
+++ server.js
@@ -2,13 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const User = require("./models/user");
+const config = require("./config/key");
 
+//applcication/x-www-form-url
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 mongoose
-  .connect(
-    "mongodb+srv://clsrns111:4565123@cluster0.vqh13.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(" db conneted"))
   .catch((err) => console.log(err));
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  //회원가입할때 필요한정보들을 클라이언트에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    lastName: req.body.lastName,
+    image: req.body.image,
+  });
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => console.log(`server connected`));
